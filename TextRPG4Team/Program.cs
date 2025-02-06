30575a4ad24517b70021963a5ab699ee123eac28
Sinneli <nyeliplays1911@gmail.com>
2025-02-06 16:59:26 +0900

    First Commit
    Contains Project

---
+++ TextRPG4Team/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextRPG4Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
